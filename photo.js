2a7ed12fbc3144c4effbf5706090bb07163c420d
Adithyan M Nair <adithyan146@gmail.com>
2023-03-12 16:03:52 +0530

    final

--- photo.js
+++ photo.js
@@ -1,17 +1,48 @@
-// Select all images in the photo album
+
 const images = document.querySelectorAll('.row img');
 
-// Add a click event listener to each image
+
 images.forEach(image => {
   image.addEventListener('click', () => {
-    // Toggle the zoom class on the clicked image
+    
     image.classList.toggle('zoom');
-
-    // Blur the background images
+    
+    
     images.forEach(otherImage => {
       if (otherImage !== image) {
         otherImage.classList.toggle('blur');
       }
     });
+
+    
+    if (image.classList.contains('zoom')) {
+      
+      images.forEach(otherImage => {
+        if (otherImage !== image) {
+          otherImage.style.pointerEvents = 'none';
+        }
+      });
+
+      
+      const button = document.createElement('button');
+      button.innerText = 'Open editor';
+      button.addEventListener('click', () => {
+        
+        window.location.href = `editor.html?src=${image.src}`;
+      });
+      image.parentElement.appendChild(button);
+    } else {
+      
+      
+      images.forEach(otherImage => {
+        otherImage.style.pointerEvents = 'auto';
+      });
+
+      
+      const button = image.parentElement.querySelector('button');
+      if (button) {
+        button.remove();
+      }
+    }
   });
 });
